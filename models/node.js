8c783fd0978680e5f21c6e31fff03e0ff392b70d
hawthorne-abendsen <hawthorne@stellar.expert>
2023-12-18 20:33:29 +0200

    domain prop added to Node model

--- models/node.js
+++ models/node.js
@@ -8,13 +8,16 @@ module.exports = class Node {
     constructor(rawNode) {
         if (!rawNode)
             throw new Error('Node is undefined')
-        const {pubkey, url} = rawNode
+        const {pubkey, url, domain} = rawNode
         if (!pubkey || !StrKey.isValidEd25519PublicKey(pubkey))
             throw new Error('pubkey is undefined or invalid')
         if (!url || !urlRegex.test(url))
             throw new Error('url is undefined or invalid')
+        if (!domain)
+            throw new Error('domain is undefined or invalid')
         this.pubkey = pubkey
         this.url = url
+        this.domain = domain
     }
 
     /**
@@ -27,16 +30,24 @@ module.exports = class Node {
      */
     url
 
+    /**
+     * @type {string}
+     */
+    domain
+
     toPlainObject() {
         return sortObjectKeys({
             pubkey: this.pubkey,
-            url: this.url
+            url: this.url,
+            domain: this.domain
         })
     }
 
     equals(other) {
         if (!(other instanceof Node))
             return false
-        return this.pubkey === other.pubkey && this.url === other.url
+        return this.pubkey === other.pubkey
+            && this.url === other.url
+            && this.domain === other.domain
     }
 }
